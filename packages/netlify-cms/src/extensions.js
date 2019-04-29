d589ac038f77b7e1bb64a9da26b611621173a651
Shawn Erquhart <shawn@erquh.art>
2019-04-29 18:51:26 -0400

    wip

---
+++ packages/netlify-cms/src/extensions.js
@@ -0,0 +1,18 @@
+import { NetlifyCmsApp as CMS } from 'netlify-cms-app/dist/esm';
+
+// Media libraries
+import uploadcare from 'netlify-cms-media-library-uploadcare';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
+
+// Code widget and default Codemirror extensions
+import NetlifyCmsWidgetCode from 'netlify-cms-widget-code';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/css/css';
+
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
+CMS.registerWidget([
+  NetlifyCmsWidgetCode.Widget(),
+]);
